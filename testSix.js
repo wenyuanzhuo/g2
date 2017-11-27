b80e823221f1f4c2f0ed05afe80808427f284ceb
wenyuanzhuo <775416111@qq.com>
2017-11-27 17:00:35 +0800

    bugall begin

---
+++ testSix.js
@@ -0,0 +1,23 @@
+function fn(n) {
+    if(n > 9) {
+      return n
+    }
+    return n + fn(n+1)
+}
+console.log(fn(1))
+
+var h = []
+var p = []
+
+function dosomething(position) {
+    for (var i = 0; i < 6; i++) {
+        if (h[i] !== 1 && position < 6) {
+            h[i] = 1
+        } else {
+            p[position] = i;
+            dosomething(position + 1)
+        }
+    }
+}
+
+dosomething(0)
